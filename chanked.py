8a27022a3734c77f224765d8ef5723d08f9543fd
Valdemar1988 <qwertysenator@gmail.com>
2023-07-01 14:04:14 +0300

    working

--- chanked.py
+++ chanked.py
@@ -1,19 +1,21 @@
 def chunked(long: int,beadroll: any) -> any:
-    n = 0
     result = []
     if len(beadroll) < long:
         result = [beadroll,]
         return result
     else:
-        for i in beadroll:
-            while n < long:
-                result = result[-1:] + i[:-1]
-                n += 1
-            result.extend(result)
+        lst = beadroll[:]
+        cash = beadroll[0:0]
+        while len(lst) > 0:
+            cash = lst[0:long]
+            result.append(cash)
+            lst = lst[long:]
         return result
 
 
-#print(chunked(2, ['a', 'b', 'c', 'd']))  # [['a', 'b'], ['c', 'd']]
-#print(chunked(3, ['a', 'b', 'c', 'd']))  # [['a', 'b', 'c'], ['d']]
-#print(chunked(3, 'foobar'))  # ['foo', 'bar']
+print(chunked(2, ['a', 'b', 'c', 'd']))  # [['a', 'b'], ['c', 'd']]
+print(chunked(3, ['a', 'b', 'c', 'd']))  # [['a', 'b', 'c'], ['d']]
+print(chunked(3, 'foobar'))  # ['foo', 'bar']
 print(chunked(10, (42,)))  # [(42,)]
+print(chunked(2, ('a', 'b', 'c', 'd')))
+print(type(('a', 'b', 'c', 'd')))
