70792a1a5af9bfd718ea4216d5b739ef4ba2a376
Valdemar1988 <qwertysenator@gmail.com>
2023-07-07 10:56:11 +0300

    fix my fibonachi and add teachers answer

--- fib.py
+++ fib.py
@@ -1,8 +1,30 @@
 def fib(n: int) -> int:
-    return int((((1 + 5 ** (0.5))/2) ** n - ((1 - 5 ** (0.5))/2) ** n) / 5 ** (0.5))
+    fi = (1 + 5 ** (0.5))/2
+    return int((fi ** n - ((- fi) ** - n)) / 5 ** (0.5))
 
 
 
 print(fib(3)) # 2
 print(fib(5)) # 5
 print(fib(10))  # 55
+
+# BEGIN
+'''def fib(num):
+    if num == 0:
+        return 0
+
+    if num == 1:
+        return 1
+
+    first, second = 0, 1
+    result = first + second
+
+    index = 2
+    while index <= num:
+        result = first + second
+        first, second = second, result
+
+        index += 1
+
+    return result'''
+# END
